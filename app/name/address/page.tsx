c61cc443452b7b7e06d976e320b0e6e99f0800ad
Dawood <dawooddogar26@gmail.com>
2023-01-07 02:54:20 +0500

    commit

---
+++ app/name/address/page.tsx
@@ -0,0 +1,5 @@
+export default function GiveAddress() {
+  return (
+    <div>I live in Lahore, pakistan </div>
+  )
+}
---
+++ app/name/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { useRouter } from "next/navigation";
+export default function GiveName() {
+  const router = useRouter();
+  return (
+    <div>
+        My name is Dawood
+        <br/>
+        <button type="button" onClick={()=>router.push('/name/address')}>
+          Get Address 
+        </button>
+    </div>
+  )
+}
